8f4cd34bf420eaa9e1dc0c3cb9d2ab8cb08d352e
SurajPawar15-smart <supersuraj143@gmail.com>
2026-02-18 09:05:43 +0530

    add todolist

---
+++ src/app/todo-list/todo-list.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [],
+  templateUrl: './todo-list.html',
+  styleUrl: './todo-list.css',
+})
+export class TodoList {
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    this.tasks.push(this.newTask);
+    this.newTask = '';
+  }
+  removeTask(index: nmber) {
+    this.tasks.splice(index, 1);
+  }
+}
